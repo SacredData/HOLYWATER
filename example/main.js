5fd2376e93409edb49f20ce19c323831d60db410
Andrew Grathwohl <andrew@sacreddata.io>
2015-11-19 16:38:45 -0500

    init

---
+++ example/main.js
@@ -0,0 +1,334 @@
+'use strict';
+
+// Create an instance
+var wavesurfer = Object.create(WaveSurfer);
+var GLOBAL_ACTIONS = {}
+
+// Init & load audio file
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        container     : document.querySelector('#waveform'),
+        waveColor     : 'violet',
+        progressColor : 'purple',
+        cursorColor   : 'navy'
+    };
+
+    if (location.search.match('scroll')) {
+        options.minPxPerSec = 100;
+        options.scrollParent = true;
+    }
+
+    // Init
+    wavesurfer.init(options);
+    // Load audio from URL
+    //wavesurfer.load('http://45.79.221.114/ch1.wav');
+
+    /* Regions */
+    wavesurfer.enableDragSelection({
+        color: randomColor(0.1)
+    });
+
+    wavesurfer.on('ready', function () {
+    if (localStorage.regions) {
+        loadRegions(JSON.parse(localStorage.regions));
+    }   // else {
+        // loadRegions(
+        //     extractRegions(
+        //         wavesurfer.backend.getPeaks(512),
+        //         wavesurfer.getDuration()
+        //     )
+        // );
+        // wavesurfer.util.ajax({
+        //     responseType: 'json',
+        //     url: 'annotations.json'
+        // }).on('success', function (data) {
+        //     loadRegions(data);
+        //     saveRegions();
+        //     });
+        // }
+    });
+    wavesurfer.on('region-click', function (region, e) {
+        e.stopPropagation();
+        // Play on click, loop on shift click
+        e.shiftKey ? region.playLoop() : region.play();
+    });
+
+    wavesurfer.on('region-click', editAnnotation);
+    wavesurfer.on('region-updated', saveRegions);
+    wavesurfer.on('region-removed', saveRegions);
+    wavesurfer.on('region-in', showNote);
+
+    wavesurfer.on('region-play', function (region) {
+        region.once('out', function () {
+            wavesurfer.play(region.start);
+            wavesurfer.pause();
+        });
+    });
+
+    /* Minimap plugin */
+    wavesurfer.initMinimap({
+        height: 30,
+        waveColor: '#ddd',
+        progressColor: '#999',
+        cursorColor: '#999'
+    });
+
+
+    /* Timeline plugin */
+    wavesurfer.on('ready', function () {
+        var timeline = Object.create(WaveSurfer.Timeline);
+        timeline.init({
+            wavesurfer: wavesurfer,
+            container: "#wave-timeline"
+        });
+    });
+
+    /**
+     * Bind controls.
+     */
+
+    GLOBAL_ACTIONS['delete-region'] = function () {
+        var form = document.forms.edit;
+        var regionId = form.dataset.region;
+        if (regionId) {
+            wavesurfer.regions.list[regionId].remove();
+            form.reset();
+        }
+    };
+
+    GLOBAL_ACTIONS['delete-all'] = function () {
+        wavesurfer.clearRegions()
+        };    
+
+    GLOBAL_ACTIONS['export'] = function () {
+        var myRecords = JSON.parse(localStorage.regions);
+        $('#my-final-table').dynatable({
+            dataset: {
+                records: myRecords
+            }
+        });
+        // window.open('data:application/json;charset=utf-8,' +
+        //     encodeURIComponent(localStorage.regions));
+    };
+
+});
+
+
+// Report errors
+wavesurfer.on('error', function (err) {
+    console.error(err);
+});
+
+// Do something when the clip is over
+wavesurfer.on('finish', function () {
+    console.log('Finished playing');
+});
+
+
+/* Progress bar */
+document.addEventListener('DOMContentLoaded', function () {
+    var progressDiv = document.querySelector('#progress-bar');
+    var progressBar = progressDiv.querySelector('.progress-bar');
+
+    var showProgress = function (percent) {
+        progressDiv.style.display = 'block';
+        progressBar.style.width = percent + '%';
+    };
+
+    var hideProgress = function () {
+        progressDiv.style.display = 'none';
+    };
+
+    wavesurfer.on('loading', showProgress);
+    wavesurfer.on('ready', hideProgress);
+    wavesurfer.on('destroy', hideProgress);
+    wavesurfer.on('error', hideProgress);
+});
+
+// Drag'n'drop
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleActive = function (e, toggle) {
+        e.stopPropagation();
+        e.preventDefault();
+        toggle ? e.target.classList.add('wavesurfer-dragover') :
+            e.target.classList.remove('wavesurfer-dragover');
+    };
+
+    var handlers = {
+        // Drop event
+        drop: function (e) {
+            toggleActive(e, false);
+
+            // Load the file into wavesurfer
+            if (e.dataTransfer.files.length) {
+                wavesurfer.loadBlob(e.dataTransfer.files[0]);
+            } else {
+                wavesurfer.fireEvent('error', 'Not a file');
+            }
+        },
+
+        // Drag-over event
+        dragover: function (e) {
+            toggleActive(e, true);
+        },
+
+        // Drag-leave event
+        dragleave: function (e) {
+            toggleActive(e, false);
+        }
+    };
+
+    var dropTarget = document.querySelector('#drop');
+    Object.keys(handlers).forEach(function (event) {
+        dropTarget.addEventListener(event, handlers[event]);
+    });
+});
+
+/**
+ * Save annotations to localStorage.
+ */
+function saveRegions() {
+    localStorage.regions = JSON.stringify(
+        Object.keys(wavesurfer.regions.list).map(function (id) {
+            var region = wavesurfer.regions.list[id];
+            return {
+                start: region.start,
+                end: region.end,
+                //attributes: region.attributes,
+                data: region.data.note
+            };
+        })
+    );
+}
+
+
+/**
+ * Load regions from localStorage.
+ */
+function loadRegions(regions) {
+    regions.forEach(function (region) {
+        region.color = randomColor(0.1);
+        wavesurfer.addRegion(region);
+    });
+}
+
+
+/**
+ * Extract regions separated by silence.
+ */
+function extractRegions(peaks, duration) {
+    // Silence params
+    var minValue = 0.0015;
+    var minSeconds = 6.5;
+
+    var length = peaks.length;
+    var coef = duration / length;
+    var minLen = minSeconds / coef;
+
+    // Gather silence indeces
+    var silences = [];
+    Array.prototype.forEach.call(peaks, function (val, index) {
+        if (val < minValue) {
+            silences.push(index);
+        }
+    });
+
+    // Cluster silence values
+    var clusters = [];
+    silences.forEach(function (val, index) {
+        if (clusters.length && val == silences[index - 1] + 1) {
+            clusters[clusters.length - 1].push(val);
+        } else {
+            clusters.push([ val ]);
+        }
+    });
+
+    // Filter silence clusters by minimum length
+    var fClusters = clusters.filter(function (cluster) {
+        return cluster.length >= minLen;
+    });
+
+    // Create regions on the edges of silences
+    var regions = fClusters.map(function (cluster, index) {
+        var next = fClusters[index + 1];
+        return {
+            start: cluster[cluster.length - 1],
+            end: (next ? next[0] : length - 1)
+        };
+    });
+
+    // Add an initial region if the audio doesn't start with silence
+    var firstCluster = fClusters[0];
+    if (firstCluster && firstCluster[0] != 0) {
+        regions.unshift({
+            start: 0,
+            end: firstCluster[firstCluster.length - 1]
+        });
+    }
+
+    // Filter regions by minimum length
+    var fRegions = regions.filter(function (reg) {
+        return reg.end - reg.start >= minLen;
+    });
+
+    // Return time-based regions
+    return fRegions.map(function (reg) {
+        return {
+            start: Math.round(reg.start * coef * 10) / 10,
+            end: Math.round(reg.end * coef * 10) / 10
+        };
+    });
+}
+
+
+/**
+ * Random RGBA color.
+ */
+function randomColor(alpha) {
+    return 'rgba(' + [
+        ~~(Math.random() * 255),
+        ~~(Math.random() * 255),
+        ~~(Math.random() * 255),
+        alpha || 1
+    ] + ')';
+
+}
+
+
+/**
+ * Edit annotation for a region.
+ */
+function editAnnotation (region) {
+    var form = document.forms.edit;
+    form.style.opacity = 1;
+    form.elements.start.value = Math.round(region.start * 10) / 10,
+    form.elements.end.value = Math.round(region.end * 10) / 10;
+    form.elements.note.value = region.data.note || '';
+    form.onsubmit = function (e) {
+        e.preventDefault();
+        region.update({
+            start: form.elements.start.value,
+            end: form.elements.end.value,
+            data: {
+                note: form.elements.note.value
+            }
+        });
+        form.style.opacity = 0;
+    };
+    form.onreset = function () {
+        form.style.opacity = 0;
+        form.dataset.region = null;
+    };
+    form.dataset.region = region.id;
+}
+
+
+/**
+ * Display annotation.
+ */
+function showNote (region) {
+    if (!showNote.el) {
+        showNote.el = document.querySelector('#subtitle');
+    }
+    showNote.el.textContent = region.data.note || '–';
+}
